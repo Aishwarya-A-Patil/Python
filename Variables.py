141d5f25005d688900cd796da1fe895d0358f2fb
PatilAishwarya29 <aishwaryapatil290397@gmail.com>
2025-10-07 22:45:07 +0530

    Variables.py

---
+++ Variables.py
@@ -0,0 +1,25 @@
+#Variable assignment
+x = 1
+y = 2
+
+#print variables
+print(x , y)
+
+#check type of variables
+print(type(x))
+z = 9.123
+print(type(z))
+a = 'test'
+print(type(a))
+
+#taking user input
+ip = input("Enter a value: ")
+print(ip)
+print(type(ip))
+
+# input() always returns a output of string type
+
+#type casting
+calc = int(input("Enter a value: "))
+print(calc)
+print(type(calc))
