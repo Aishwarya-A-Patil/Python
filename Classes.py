dcde434d9c9e2e2bae759254a4743f22a0995ad3
PatilAishwarya29 <aishwaryapatil290397@gmail.com>
2025-10-09 09:56:34 +0530

    Classes.py --> Attributes

--- Classes.py
+++ Classes.py
@@ -1,3 +1,5 @@
+
+#class
 class Phone:                                        # class defination
     def makeCall(self):
         print("Making Call")                        #method/ function/ object
@@ -9,3 +11,54 @@ class Phone:                                        # class defination
 p1 = Phone();                                      # class initialization
 p1.makeCall()
 p1.playingGame()
+
+
+#Attributes
+class Phone1 :
+    def set_color(self,color):
+        self.color = color
+
+    def set_cost(self, cost):
+        self.cost = cost
+
+    def show_color(self):
+        print("The color is ", self.color)
+        print("The cost is ", self.cost)
+
+    def show_cost(self):
+        print("The color is : ", self.color)
+        print("The cost is : ", self.cost)
+
+
+p2 = Phone1()
+p2.set_color("Black")
+p2.set_cost(10000)
+p2.show_color()
+p2.show_cost()
+
+
+class Operation:
+    def firstValue(self, value1):
+        self.val1 = value1
+       #print("val3 = ",val3) -----> This will throw error as val3 is not accessible
+
+    def secondValue(self, value2, value3):
+        self.val2 = value2
+        val3  = 123
+       #print(val3)
+        
+
+    def additionOP(self):
+        addResult = self.val1 + self.val2 
+        #only self.something is accessible all over the class
+        #the valiable addResult is only available accessible inside additionOP
+        print("The result of addition operation is: ", addResult)
+
+
+p3 = Operation()
+p3.firstValue(1)
+p3.secondValue(2,3)
+p3.additionOP()
+
+
+
