4b348a877f9a32c7c5ca0ee5ded3fe675f75a3a2
PatilAishwarya29 <aishwaryapatil290397@gmail.com>
2025-10-09 09:21:22 +0530

    Classes.py

---
+++ Classes.py
@@ -0,0 +1,11 @@
+class Phone:                                        # class defination
+    def makeCall(self):
+        print("Making Call")                        #method/ function/ object
+
+    def playingGame(self):
+        print("Playing Games.")
+
+
+p1 = Phone();                                      # class initialization
+p1.makeCall()
+p1.playingGame()
