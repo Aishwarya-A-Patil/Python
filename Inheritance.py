6108818d2e2beecc6ccfbef5661ec36f10b7be32
PatilAishwarya29 <aishwaryapatil290397@gmail.com>
2025-10-14 09:31:52 +0530

    Added Example 2 to Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -19,3 +19,19 @@ class Car(Vehicle):
 car = Car(50, 50000)
 #car.showCarDetails()
 car.showVehicleDetails()  # inherited from Vehicle class
+
+
+#Example 2
+class Math1:
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+        self.addResult = self.n1 + self.n2
+        
+class Math2(Math1):
+    def showResult(self):
+        print("Addition Result: ", self.addResult)
+
+
+m2 = Math2(1, 3)
+m2.showResult()
